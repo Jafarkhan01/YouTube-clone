9b3e35107f3c7146124acab124991ef749f0b403
Команда Разработки <afkor.devteam@gmail.com>
2025-03-06 16:58:41 +0500

    Work in project : create a components, img and icons

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
-import { HeaderComponent } from "./header/header.component";
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { HeaderComponent } from "../header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { ShortsComponent } from "./shorts/shorts.component";
 import { SubscriptionsComponent } from "./subscriptions/subscriptions.component";
 import { TrendingComponent } from "./trending/trending.component";
 import { GamingComponent } from "./gaming/gaming.component";
+import { MusicComponent } from "./music/music.component";
+import { LiveComponent } from "./live/live.component";
+import { SportComponent } from "./sport/sport.component";
+import { EducationComponent } from "./education/education.component";
 
 @Component({
   selector: 'app-pages',
-  imports: [NavBarComponent, HeaderComponent, TrendingComponent, HomeComponent, ShortsComponent, SubscriptionsComponent, GamingComponent],
+  imports: [HeaderComponent, EducationComponent],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
